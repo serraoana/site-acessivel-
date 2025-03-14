337fe5677ec66ceeb7b970d2c7ef74bca1b7d683
serraoana <144172636+serraoana@users.noreply.github.com>
2025-03-14 17:39:17 +0000

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
