08ad01b9fc3a6ef64ce7ca3b9b0c847cd2172103
serraoana <144172636+serraoana@users.noreply.github.com>
2025-03-28 19:34:33 +0000

    contraste p3

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
+    const alternaContraste = this.getElementById('alterna-contraste')
+
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
@@ -18,6 +20,8 @@ document.addEventListener('DOMContentLoaded', function(){
             tamanhoAtualFonte -= 0.1;
             document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
             });
-    
+            alternaContraste.addEventListener('click', funtion(){
+                document.body.classList.toggle('alto-contraste')    
+            })
 
 });
