63e628fefe33f24164e8bc93f0f9498c7ec98651
serraoana <144172636+serraoana@users.noreply.github.com>
2025-04-04 19:18:29 +0000

    navegação

--- script.js
+++ script.js
@@ -25,3 +25,8 @@ document.addEventListener('DOMContentLoaded', function(){
             })
 
 });
+
+ScrollReveal().reveal('#inicio',{delay:500});
+ScrollReveal().reveal('#tropicalia',{delay:500});
+ScrollReveal().reveal('#galeria',{delay:500});
+ScrollReveal().reveal('#contato',{delay:500});
